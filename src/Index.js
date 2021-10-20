87a6176c5cba67e87dfaee3afd41181f641ed852
Roman-wdesign <gabbasov.tomsk@gmail.com>
2021-10-20 10:55:11 +0700

    icons has been changed

--- src/Index.js
+++ src/Index.js
@@ -1,16 +1,3 @@
-
-
-
 import './styles/styles.css'
 import './styles/scss/style.scss'
-
-
-
-
-
-//console.log($.fn.slick);
-
-/*$(document).ready(function($) {
-    $('body').css('background-color','orange');
-});
-*/
+// import 'bootstrap'
